a5a6329058f5e7c19abb813cfd666c60dd66a7a9
CaptainJiggles11 <54753626+CaptainJiggles11@users.noreply.github.com>
2021-07-16 09:00:16 -0500

    test
    e

---
+++ Mixed's Unity Proj/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public Vector2 speed = new Vector2(50, 50);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(speed.x * inputX, speed.y * inputY, 0);
+
+        movement *= Time.deltaTime;
+
+        transform.Translate(movement);
+    }
+}
---
+++ Mixed's Unity Proj/Assets/Scripts/spawnAsteroid.cs
@@ -0,0 +1,62 @@
+// Instantiates 10 copies of Prefab each 2 units apart from each other
+
+using UnityEngine;
+using System.Collections;
+
+public class spawnAsteroid : MonoBehaviour
+{
+    //Array of objects to spawn (note I've removed the private goods variable)
+    public GameObject[] theGoodies;
+
+    //Time it takes to spawn theGoodies
+    [Space(3)]
+    public float waitingForNextSpawn = 10;
+    public float theCountdown = 10;
+
+    // the range of X
+    [Header("X Spawn Range")]
+    public float xMin;
+    public float xMax;
+
+    // the range of y
+    [Header("Y Spawn Range")]
+    public float yMin;
+    public float yMax;
+
+
+    void Start()
+    {
+    }
+
+    public void Update()
+    {
+        // timer to spawn the next goodie Object
+        theCountdown -= Time.deltaTime;
+        if (theCountdown <= 0)
+        {
+            SpawnGoodies();
+            theCountdown = waitingForNextSpawn;
+        }
+    }
+
+
+    void SpawnGoodies()
+    {
+        // Defines the min and max ranges for x and y
+        Vector2 pos = new Vector2(Random.Range(xMin, xMax), Random.Range(yMin, yMax));
+
+        // Choose a new goods to spawn from the array (note I specifically call it a 'prefab' to avoid confusing myself!)
+        GameObject goodsPrefab = theGoodies[Random.Range(0, theGoodies.Length)];
+
+        // Creates the random object at the random 2D position
+
+        GameObject newGoods = (GameObject)Instantiate(goodsPrefab, pos, transform.rotation);
+
+        newGoods.GetComponent<Rigidbody>().AddForce(Random.Range(-10,10), Random.Range(-10, 10), 0);
+        newGoods.transform.Rotate(0, 0, 50 * Time.deltaTime);
+
+        // If I wanted to get the result of instantiate and fiddle with it, I might do this instead:
+        //GameObject newGoods = (GameObject)Instantiate(goodsPrefab, pos)
+        //newgoods.something = somethingelse;
+    }
+}
